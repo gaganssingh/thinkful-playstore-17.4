41d6ec8f886a847a0550629e9ec71dc6cae845f4
Gagan Singh <gagansingh.dev@outlook.com>
2019-12-11 18:26:47 -0500

    Added server/backend files

---
+++ ps-server/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const playstore = require("./playstore-data");
+
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/apps", (req, res) => {
+	const { sort, genres } = req.query;
+	let results = playstore;
+
+	if (sort) {
+		if (
+			![
+				"rating",
+				"app",
+			].includes(sort)
+		) {
+			return res.status(400).send("sort must be one of rating or app");
+		}
+	}
+	if (sort === "app") {
+		results = store.sort((a, b) => {
+			return a["App"].toLowerCase() > b["App"].toLowerCase()
+				? 1
+				: a["App"].toLowerCase() < b["App"].toLowerCase() ? -1 : 0;
+		});
+	} else if (sort === "rating") {
+		results = results.sort((a, b) => {
+			return a["Rating"] < b["Rating"] ? 1 : a["Rating"] > b["Rating"] ? -1 : 0;
+		});
+	}
+
+	if (genres) {
+		if (
+			![
+				"action",
+				"puzzle",
+				"strategy",
+				"casual",
+				"arcade",
+				"card",
+			].includes(genres.toLowerCase())
+		) {
+			return res.status(400).send("Genre must be one of Action, Puzzle, Strategy, Casual, Arcade or Card");
+		}
+		results = results.filter((app) => {
+			return app.Genres.toLowerCase() === genres.toLowerCase();
+		});
+	}
+
+	// return res.status(200).send(results);
+
+	res.status(200).json(results);
+});
+
+app.listen(8000, () => console.log("Server started at port 8000"));
